fcd98f141ad5c98ff853320d4d100593627d45c9
Keshav Kumar <keshavkumar8122@gmail.com>
2023-02-08 23:45:23 +0530

    Added title and removed un wanted imports
    Added title and removed un wanted imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
 import ContactUs from './Components/ContactUs';
 import Footer from './Components/Footer';
 import Gallery from './Components/Gallery';
 import Navbar from './Components/Navbar';
+import Slider from './Components/Slider';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Home></Home>
+      <Slider></Slider>
       <Gallery></Gallery>
+      <About></About>
       <ContactUs></ContactUs>
       <Footer></Footer>
     </div>
