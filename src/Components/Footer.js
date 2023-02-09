a912f2516d365415b90f1aaefbc2ca0fa1336ea2
Keshav Kumar <keshavkumar8122@gmail.com>
2023-02-09 20:46:47 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
                     <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
                         <h3 className="fs-6 text-white">Services</h3>
                         <ul className="list-unstyled">
-                            <li><a className="link-light" href="#">Web design</a></li>
-                            <li><a className="link-light" href="#">Development</a></li>
-                            <li><a className="link-light" href="#">Hosting</a></li>
+                            <li><a className="link-light" href="#">Art Store</a></li>
+                            <li><a className="link-light" href="#">Painitng</a></li>
+                            <li><a className="link-light" href="#">Hand Painting</a></li>
                         </ul>
                     </div>
                     <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
@@ -21,25 +21,17 @@ function Footer() {
                             <li><a className="link-light" href="#">Legacy</a></li>
                         </ul>
                     </div>
-                    <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                        <h3 className="fs-6 text-white">Careers</h3>
-                        <ul className="list-unstyled">
-                            <li><a className="link-light" href="#">Job openings</a></li>
-                            <li><a className="link-light" href="#">Employee success</a></li>
-                            <li><a className="link-light" href="#">Benefits</a></li>
-                        </ul>
-                    </div>
                     <div className="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last item social">
                         <div className="fw-bold d-flex align-items-center mb-2"><span className="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2"><svg className="bi bi-bezier fs-5" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
                             <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
-                        </svg></span><span>Brand</span></div>
+                        </svg></span><span>Home</span></div>
                         <p className="text-muted copyright">Sem eleifend donec molestie, integer quisque orci aliquam.</p>
                     </div>
                 </div>
                 <hr />
                 <div className="d-flex justify-content-between align-items-center pt-3">
-                    <p className="mb-0">Copyright © 2023 Brand</p>
+                    <p className="mb-0">Copyright © Art Store</p>
                     <ul className="list-inline mb-0">
                         <li className="list-inline-item"><svg className="bi bi-facebook" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
