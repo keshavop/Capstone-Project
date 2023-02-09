777457d454e48e77a1d5ba1dd1fc67710ad6f125
Keshav Kumar <keshavkumar8122@gmail.com>
2023-02-09 19:46:21 +0530

    Home Section
    Designed Home Section

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+
+function Home() {
+    return (
+        <div className='bg' id='home'>
+            <h1 className='heading'>Art Store 🎨</h1>
+
+            <h1 className='quote'></h1>
+
+            <h1 className='order'>Order your painting now..</h1>
+        </div>
+    )
+}
+
+export default Home
